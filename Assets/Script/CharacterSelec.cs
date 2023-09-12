5cd85c390cfbd9eee63cdc9a1a39faeffc20af28
SleepyLava <sleepylavagames@gmail.com>
2023-09-12 15:18:47 +0800

    first commit

---
+++ Assets/Script/CharacterSelec.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelec : MonoBehaviour
+{
+    [SerializeField] RectTransform[] charBtns;
+    [SerializeField] RectTransform indicator;
+    [SerializeField] float moveDelay;
+
+    public int indicatorPos;
+    public float moveTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveTimer <= moveDelay)
+        {
+            moveTimer += Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (moveTimer > moveDelay)
+            {
+                if (indicatorPos < charBtns.Length - 1)
+                {
+                    indicatorPos++;
+                }
+                else
+                {
+                    indicatorPos = 0;
+                }
+                moveTimer = 0;
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (moveTimer > moveDelay)
+            {
+                if (indicatorPos > 0)
+                {
+                    indicatorPos--;
+                }
+                else
+                {
+                    indicatorPos = charBtns.Length - 1;
+                }
+                moveTimer = 0;
+            }
+        }
+        indicator.localPosition = charBtns[indicatorPos].localPosition;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Character " + (indicatorPos + 1));
+        }
+    }
+
+    public void HoverOnButton(int btnPos)
+    {
+        indicatorPos = btnPos;
+    }
+
+    public void ButtonPress()
+    {
+        Debug.Log("Character " + (indicatorPos + 1));
+    }
+}
